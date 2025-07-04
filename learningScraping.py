b7ad21478e985b2076ec39a351ed0fffb0a756d8
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-04 15:13:18 +0100

    sdf

--- learningScraping.py
+++ learningScraping.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen as u
+import requests
 from bs4 import BeautifulSoup as soup
 
 my_url = "https://www.newegg.com/p/pl?d=graphic+cards"
+request = requests.get(my_url, headers={'User-Agent': 'Mozilla/5.0'})
 print(my_url)
